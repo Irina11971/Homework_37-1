fee98d783cced4d4fec221da6fc4539ce79e03e5
Sushkov AS <aveslaizer@gmail.com>
2023-03-05 21:09:39 +0300

    Реализована функция bubblesorting

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-
-
 def bubble_sorting(alist: list) -> list:
-    pass
+    """
+    Возвращает список, отсортированый пузырьковой сортировкой
+
+    :param alist (list): Список для сортировки
+    :return:
+            alist (list): Отсортированный список
+    """
+    n = len(alist)
+    for i in range(n - 1):
+        swapped = True
+        for j in range(n - i - 1):
+            if alist[j] > alist[j + 1]:
+                alist[j], alist[j + 1] = alist[j + 1], alist[j]
+                swapped = False
+        if swapped:
+            break
+    return alist
 
 
 def execute_application():
